de4b15c4c08ee71deff685f8eeaeac0e32accf4d
happyliuyi <35760863+happyliuyi@users.noreply.github.com>
2020-01-20 19:09:32 +0800

    解决 http://localhost:8080/swagger-ui.html页面打不开问题
    参考 https://blog.csdn.net/miachen520/article/details/95718639

--- springboot-swagger/src/main/java/com/example/swagger/SwaggerApplication.java
+++ springboot-swagger/src/main/java/com/example/swagger/SwaggerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSwagger2  //不加此注解，则http://localhost:8080/swagger-ui.html页面可能打不开
 public class SwaggerApplication {
 
     public static void main(String[] args) {
