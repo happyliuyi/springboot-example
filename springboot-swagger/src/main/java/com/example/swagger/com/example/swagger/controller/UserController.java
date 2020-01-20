e605da82ec69ba407d0cf6a44fc1009b511b6f16
happyliuyi <35760863+happyliuyi@users.noreply.github.com>
2020-01-20 19:54:27 +0800

    paramType
    尝试paramType各种value

--- springboot-swagger/src/main/java/com/example/swagger/com/example/swagger/controller/UserController.java
+++ springboot-swagger/src/main/java/com/example/swagger/com/example/swagger/controller/UserController.java
@@ -2,7 +2,6 @@ package com.example.swagger.com.example.swagger.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,28 +11,41 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import java.util.logging.Logger;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @RestController
-@RequestMapping("/api/user")
-@Api(tags = "用户控制器")
-public class UserController {
+@RequestMapping("/art")
+@Api(tags = "文章控制器")
+public class ArticleController {
 
-    @ApiOperation(value = "打招呼", notes = "测试方法")
-    @RequestMapping(value = "/sayhi", method = RequestMethod.POST)
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "name", value = "姓名", required = true, paramType = "form"),
-            @ApiImplicitParam(name = "age", value = "年龄", required = true, paramType = "form"),
-    })
-    public String sayHi(String name, int age) {
-        return "Hello," + name;
-    }
-
-    @ApiOperation(value = "获取所有用户", notes = "查询分页数据")
+    @ApiOperation(value = "获取所有文章", notes = "查询分页数据")
     @RequestMapping(value = "/getall", method = RequestMethod.POST)
     public String getAll() {
-        return "所有用户";
+        return "所有文章";
     }
+
+
+    @ApiOperation(value = "获取单篇文章", notes = "获取单篇文章")
+    @RequestMapping(value = "/getone/{title}", method = RequestMethod.GET)
+    @ApiImplicitParam(name = "title", value = "文章名", required = true, paramType = "path") //http://localhost:8080/art/getone/{title}
+    public String getOne(String title) {
+        return "单篇";
+    }
+
+    
+    @ApiOperation(value = "查询文章作者名", notes = "获取文章篇文章")
+    @RequestMapping(value = "/getAuthor", method = RequestMethod.POST)
+    @ApiImplicitParam(name = "title", value = "文章名", required = true, paramType = "form")
+    public String getAuthor(String title) {
+        return "Tracy";
+    }
+    
+    @ApiOperation(value = "查询某个作者的文章", notes = "获取文章篇文章")
+    @RequestMapping(value = "/getAuthorBooks", method = RequestMethod.GET)
+    @ApiImplicitParam(name = "AuthorName", value = "文章名", required = true, paramType = "query")  //即query 是查询路径带？的 eg:http://localhost:8080/art/getAuthorBooks?AuthorName=lucy
+    public String getAuthorBooks(String AuthorName) {
+        return "Tracy' life";
+    }
+
 }
